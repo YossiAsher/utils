55363eced3595391a04ce50975eb7dbdd65cf8ed
Yossi Asher <yossi.asher@gmail.com>
2021-10-07 16:58:40 +0300

    fix

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -2,31 +2,24 @@ import wandb
 import numpy as np
 from tensorflow.keras.callbacks import Callback
 
-TABLE_NAME = "predictions"
-PROJECT_NAME = 'svg-attention6'
-RUN_NAME = ''
-wandb.login()
-
 
 class ValLog(Callback):
-    """ Custom callback to log validation images
-    at the end of each training epoch"""
 
-    def __init__(self, dataset=None):
+    def __init__(self, dataset=None, table_name="predictions", project_name="svg-attention6", run_name=""):
         super().__init__()
         self.dataset = dataset
-        self.run = wandb.init(project=PROJECT_NAME, job_type="inference", name=RUN_NAME)
+        self.table_name = table_name
+        self.run = wandb.init(project=project_name, job_type="inference", name=run_name)
 
     def on_epoch_end(self, epoch, logs=None):
-        columns = ["file", "prediction", "target"]
-
+        columns = ["file", "target", "prediction"]
         predictions_table = wandb.Table(columns=columns)
-
-        # log image, predicted and actual labels, and all scores
+        
         for X, y, files, paths_list in self.dataset.epoc_data.values():
             val_preds = self.model.predict(X)
             for index, x in enumerate(X):
+                target = self.dataset.classes[y[index]]
                 prediction = self.dataset.classes[np.argmax(val_preds[index])]
-                row = [files[index], prediction, self.dataset.classes[y[index]]]
+                row = [files[index], target, prediction]
                 predictions_table.add_data(*row)
-        self.run.log({TABLE_NAME: predictions_table})
+        self.run.log({self.table_name: predictions_table})
