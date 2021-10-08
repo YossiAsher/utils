dccb2fb9439a29534a65de4cd0a4acac07373c8b
Yossi Asher <yossi.asher@gmail.com>
2021-10-08 10:57:08 +0300

    add images

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -5,14 +5,14 @@ from tensorflow.keras.callbacks import Callback
 
 class ValLog(Callback):
 
-    def __init__(self, dataset=None, table_name="predictions", project_name="svg-attention6", run_name=""):
+    def __init__(self, dataset=None, table="predictions", project="svg-attention6", run=""):
         super().__init__()
         self.dataset = dataset
-        self.table_name = table_name
-        self.run = wandb.init(project=project_name, job_type="inference", name=run_name)
+        self.table_name = table
+        self.run = wandb.init(project=project, job_type="inference", name=run)
 
     def on_epoch_end(self, epoch, logs=None):
-        columns = ["file", "target", "prediction"]
+        columns = ["epoch", "dataset", "file", "target", "prediction"]
         predictions_table = wandb.Table(columns=columns)
         
         for X, y, files, paths_list in self.dataset.epoc_data.values():
@@ -20,6 +20,6 @@ class ValLog(Callback):
             for index, x in enumerate(X):
                 target = self.dataset.classes[y[index]]
                 prediction = self.dataset.classes[np.argmax(val_preds[index])]
-                row = [files[index], target, prediction]
+                row = [epoch, self.dataset.name, files[index], target, prediction]
                 predictions_table.add_data(*row)
         self.run.log({self.table_name: predictions_table})
