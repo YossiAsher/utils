c389d061378fca0b5495691c999f93adfa882faf
Yossi Asher <yossi.asher@gmail.com>
2021-10-08 14:55:09 +0300

    add Image to callback (#2)
    * add images to callback

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,3 +1,6 @@
+import os.path
+import glob
+
 import wandb
 import numpy as np
 from tensorflow.keras.callbacks import Callback
@@ -5,21 +8,29 @@ from tensorflow.keras.callbacks import Callback
 
 class ValLog(Callback):
 
-    def __init__(self, dataset=None, table_name="predictions", project_name="svg-attention6", run_name=""):
+    def __init__(self, dataset=None, table="predictions", project="svg-attention6", run=""):
         super().__init__()
         self.dataset = dataset
-        self.table_name = table_name
-        self.run = wandb.init(project=project_name, job_type="inference", name=run_name)
+        self.table_name = table
+        self.run = wandb.init(project=project, job_type="inference", name=run)
 
     def on_epoch_end(self, epoch, logs=None):
-        columns = ["file", "target", "prediction"]
+        columns = ["epoch", "dataset", "file", "svg", "target", "prediction"]
         predictions_table = wandb.Table(columns=columns)
-        
-        for X, y, files, paths_list in self.dataset.epoc_data.values():
-            val_preds = self.model.predict(X)
+
+        for i in range(len(self.dataset)):
+            epoc_path_index = os.path.join(self.dataset.epoc_path.name, str(i))
+            data_path = os.path.join(epoc_path_index, 'data.npz')
+            loaded = np.load(data_path)
+            X = loaded['X']
+            y = loaded['y']
+            predictions = self.model.predict(X)
             for index, x in enumerate(X):
                 target = self.dataset.classes[y[index]]
-                prediction = self.dataset.classes[np.argmax(val_preds[index])]
-                row = [files[index], target, prediction]
+                prediction = self.dataset.classes[np.argmax(predictions[index])]
+                png_file = glob.glob(f'{epoc_path_index}/{index}/**/*.png', recursive=True)[0]
+                file = png_file[png_file.index(png_file.split('/')[-2]):]
+                row = [epoch, self.dataset.name, file, wandb.Image(png_file), target, prediction]
                 predictions_table.add_data(*row)
         self.run.log({self.table_name: predictions_table})
+        self.dataset.clean_epoc_path()
