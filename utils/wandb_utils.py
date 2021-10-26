9098640d67613f2f667337cfdd869bb2e9563bcf
Yossi Asher <yossi.asher@gmail.com>
2021-10-26 21:51:56 +0300

    fix

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -1,4 +1,9 @@
 import wandb
+import glob
+import os
+
+import cairosvg
+import numpy as np
 
 from utils.sequences import DataGenerator
 
@@ -14,3 +19,37 @@ def get_datasets(project, artifact, batch_size, dim_size, input_shape):
                                  input_shape=input_shape, shuffle=False, supervised=True)
 
     return train_dataset, test_dataset
+
+
+def create_raw_data(project, artifact, path):
+    raw_files = glob.glob(f"{path}/**/*.svg", recursive=True)
+    run = wandb.init(project=project, job_type="upload", reinit=True)
+    raw_data_at = wandb.Artifact(artifact, type="raw_data")
+
+    for file_path in raw_files:
+        label = file_path.split('/')[-2]
+        file = file_path.split('/')[-1]
+        raw_data_at.add_file(file_path, name=label + "/" + file)
+
+    run.log_artifact(raw_data_at)
+
+
+def split_data(project, raw_artifact, split_artifact):
+    run = wandb.init(project=project, job_type="data_split", reinit=True)
+    data_at = run.use_artifact(raw_artifact + ":latest")
+    data_dir = data_at.download()
+    data_split_at = wandb.Artifact(split_artifact, type="balanced_data")
+
+    labels = os.listdir(data_dir)
+    for label in labels:
+        images_per_label = os.listdir(os.path.join(data_dir, label))
+        for img_file in images_per_label:
+            img_id = img_file.split('.')[0]
+            full_path = os.path.join(data_dir, label, img_file)
+            split = "test" if np.random.rand() > 0.8 else "train"
+            data_split_at.add_file(full_path, name=f"svg/{split}/{label}/{img_id}.svg")
+            png_full_path = full_path.replace('.svg', '.png')
+            cairosvg.svg2png(url=full_path, write_to=png_full_path, parent_width=100, parent_height=100)
+            data_split_at.add_file(png_full_path, name=f"png/{split}/{label}/{img_id}.png")
+
+    run.log_artifact(data_split_at)
